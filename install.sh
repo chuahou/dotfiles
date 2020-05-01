618947f5c5771d5a51e91c7f96401eab9582ea42
Chua Hou <human+github@chuahou.dev>
2020-05-01 12:27:23 +0800

    Fix handling of different types of ARGBACKUP

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 
 print_usage ()
 {
-	echo "Usage: ./install.sh -o [HOME] -i [IGNORE]"
+	echo "Usage: ./install.sh -o [HOME] -i [IGNORE] -b [BACKUP]"
 	echo "    [HOME]: base directory (usually ~) to install symlinks in"
 	echo "    [IGNORE]: file with paths to ignore"
 	echo "    [BACKUP]: directory to backup to"
@@ -16,10 +16,10 @@ print_usage ()
 # defaults
 ARGHOME=$HOME
 ARGIGNORE=.install_ignore
-ARGBACKUP=.dotfiles_backup
+ARGBACKUP=$(pwd)/.dotfiles_backup
 
 # get options
-while getopts 'o:i:' OPTION; do
+while getopts 'o:i:b:' OPTION; do
 	case "$OPTION" in
 		o)
 			ARGHOME="$OPTARG"
@@ -50,6 +50,9 @@ if [ ! -e "$ARGIGNORE" ]; then
 fi
 
 # ensure backup directory exists, creating it otherwise
+if [[ ! "$ARGBACKUP" =~ ^/ ]]; then # check if ARGBACKUP is absolute path
+	ARGBACKUP=$(pwd)/$ARGBACKUP # if not, make it absolute relative to pwd
+fi
 mkdir -p $ARGBACKUP
 
 # get list of dotfiles with absolute path
