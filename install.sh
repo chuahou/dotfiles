c22c0b59f256cc8a84905cc7cab51e4f5d2c8def
Chua Hou <human+github@chuahou.dev>
2020-05-01 11:58:33 +0800

    Add install.sh (up to generating list of files)

---
+++ install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+# installs all current files in this directory to [HOME], ignoring paths matched
+# by paths listed in [IGNORE], and backup originals (if any) to [BACKUP]
+#
+# ./install.sh -o(utput) [HOME] -i(gnore) [IGNORE] -b(ackup) [BACKUP]
+
+print_usage ()
+{
+	echo "Usage: ./install.sh -o [HOME] -i [IGNORE]"
+	echo "    [HOME]: base directory (usually ~) to install symlinks in"
+	echo "    [IGNORE]: file with paths to ignore"
+	echo "    [BACKUP]: directory to backup to"
+}
+
+# defaults
+ARGHOME=$HOME
+ARGIGNORE=.install_ignore
+ARGBACKUP=.dotfiles_backup
+
+# get options
+while getopts 'o:i:' OPTION; do
+	case "$OPTION" in
+		o)
+			ARGHOME="$OPTARG"
+			;;
+		i)
+			ARGIGNORE="$OPTARG"
+			;;
+		b)
+			ARGBACKUP="$OPTARG"
+			;;
+		?)
+			print_usage
+			exit 1
+			;;
+	esac
+done
+
+# ensure output directory exists and is directory
+if [ ! -d "$ARGHOME" ]; then
+	>&2 echo "$ARGHOME is not a directory"
+	exit 1
+fi
+
+# ensure ignore file is a valid file
+if [ ! -e "$ARGIGNORE" ]; then
+	>&2 echo "$ARGIGNORE could not be read"
+	exit 1
+fi
+
+# ensure backup directory exists, creating it otherwise
+mkdir -p $ARGBACKUP
+
+# get list of dotfiles with absolute path
+IGNOREFILES=$(cat $ARGIGNORE | \
+	sed -r "s|/\./|/|g" | \
+	sed -r "s|^\./||g") # get paths to ignore and remove excess "./"s
+DOTFILES=$(echo "$IGNOREFILES" | \
+	xargs printf "! -path ./%s " | \
+	xargs find . -type f | \
+	sed -r "s|^\./||g") # get paths of DOTFILES and ignore paths, then remove ./
